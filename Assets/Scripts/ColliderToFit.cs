e81d0998f817bdf94d282b893725c9c85aea965c
Onur Han <71465720+OnurHanGumus@users.noreply.github.com>
2022-05-21 06:15:12 +0300

    Mevcut Proje
    -Mevcut proje olduğu gibi yüklendi,
    -Mülakatta gösterilen projenin azıcık Picker3D'ye benzetilmiş hâlidir.

---
+++ Assets/Scripts/ColliderToFit.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ColliderToFit : MonoBehaviour
+{
+    public void Start()
+    {
+        FitToChildren();
+    }
+    void FitToChildren()
+    {
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+        GameObject rootGameObject = gameObject;
+
+        for (int i = 0; i < rootGameObject.transform.childCount; ++i)
+        {
+            Renderer childRenderer = rootGameObject.transform.GetChild(i).GetComponent<Renderer>();
+            if (childRenderer != null)
+            {
+                
+                bounds.Encapsulate(childRenderer.bounds);
+                
+            }
+        }
+
+        BoxCollider collider = (BoxCollider)rootGameObject.GetComponent<Collider>();
+        collider.center = bounds.center - rootGameObject.transform.position;
+        collider.size = bounds.size;
+
+    } 
+}
+
